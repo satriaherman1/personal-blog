8476c222adaa2e653425445a419a44df2c234255
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-07-12 16:36:38 +0700

    fix rendering theme disqus

--- components/DisqusComments.tsx
+++ components/DisqusComments.tsx
@@ -1,6 +1,8 @@
 import { DiscussionEmbed } from 'disqus-react'
 import { GhostPostOrPage } from '@lib/ghost'
 import { resolvePostFullPath } from '@utils/routing'
+import { useEffect, useRef, useState } from 'react'
+import { useTheme } from '@components/contexts/themeProvider'
 
 interface DisqusCommentsProps {
   post: GhostPostOrPage
@@ -13,9 +15,16 @@ export const DisqusComments = ({ post, shortname, siteUrl }: DisqusCommentsProps
   const { id: identifier, title } = post
   const config = { url, identifier, title }
 
+  const { dark } = useTheme()
+  const [forceUpdateKey, setForceUpdateKey] = useState<number>(0)
+
+  useEffect(() => {
+    setForceUpdateKey(forceUpdateKey + 1)
+  }, [dark])
+
   return (
     <section>
-      <DiscussionEmbed {...{ shortname, config }} />
+      <DiscussionEmbed key={forceUpdateKey} {...{ shortname, config }} />
     </section>
   )
 }
