89e2a146c8ce58a7cffb6d8c6afce9865caacb2c
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-08-24 23:12:12 +0700

    fix search system

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -24,36 +24,33 @@ const SearchBar = ({ placeholder, data: { posts } }: ISearchBar) => {
   }, [searchInput])
 
   return (
-    <section className="search-container">
-      <input
-        type="text"
-        className="search-bar"
-        onFocus={() => setShowResult(true)}
-        onBlur={() => setShowResult(false)}
-        onChange={({ target: { value } }) => setSearchInput(value)}
-        placeholder={placeholder}
-      />
+    <>
+      <section className="search-container">
+        <input type="text" className="search-bar" onFocus={() => setShowResult(true)} onChange={({ target: { value } }) => setSearchInput(value)} placeholder={placeholder} />
 
-      {showResult && (
-        <div className="search-result-box">
-          {searchResult.length === 0 ? (
-            <h4 className="text-center no-result">tidak ada hasil</h4>
-          ) : (
-            searchResult.map((data: any) => (
-              <div key={data.uuid}>
-                <Image src={data.featureImage.url} height={40} width={40} alt={data.title} />
-                <div className="search-result-box-text">
-                  <h4>
-                    <Link href={`/${data.slug}`}>{data.title}</Link>
-                  </h4>
-                  {/* <p>{formatService.truncateText(data.excerpt, 40)}</p> */}
+        {showResult && (
+          <div className="search-result-box">
+            {searchResult.length === 0 ? (
+              <h4 className="text-center no-result">tidak ada hasil</h4>
+            ) : (
+              searchResult.map((data: any) => (
+                <div key={data.uuid}>
+                  <Image src={data.featureImage.url} height={40} width={40} alt={data.title} />
+                  <div className="search-result-box-text">
+                    <h4>
+                      <Link href={`/${data.slug}`}>{data.title}</Link>
+                    </h4>
+                    {/* <p>{formatService.truncateText(data.excerpt, 40)}</p> */}
+                  </div>
                 </div>
-              </div>
-            ))
-          )}
-        </div>
-      )}
-    </section>
+              ))
+            )}
+          </div>
+        )}
+      </section>
+
+      <div className="search-before" onClick={() => setShowResult(false)}></div>
+    </>
   )
 }
 
